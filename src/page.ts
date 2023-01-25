11bf47f270942da1e1a9a7da212c354201f10655
vanyapr <snowflax@ya.ru>
2023-01-25 13:42:30 +0300

    refactor: Убрать лишние комментарии

--- src/page.ts
+++ src/page.ts
@@ -1,15 +1,10 @@
 const utmDisplayArea = document.getElementById('data');
 
-console.log(utmDisplayArea)
-
 const displayCurrentUtmStore = () => {
-  console.log('Апдейт стора?')
-
   const data = localStorage.getItem('utm-history-list');
 
   if (data && utmDisplayArea) {
     utmDisplayArea.innerHTML = data.toString().replaceAll('],', '], \n\n\r');
-    // utmDisplayArea.innerHTML = JSON.stringify(data, null, '  ');
   }
 }
 
